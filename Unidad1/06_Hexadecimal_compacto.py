e5c9e6f904bda97b19650f902ce3624ad34ba194
Cristian Yael Osornio Cerecedo <cristiancer412@gmail.com>
2026-09-01 10:49:07 -0600

    Folders structure

--- Unidad1/06_Hexadecimal_compacto.py
+++ Unidad1/06_Hexadecimal_compacto.py
@@ -1,5 +1,5 @@
-numero, hexadecimal= "10", ""
-if numero == "0":  print("0")
-digitos = "0123456789ABCDEF"
-while numero > "0": hexadecimal, numero = digitos[int(numero) % 16] + hexadecimal, str(int(numero) // 16)
-print(hexadecimal)
+numero, hexadecimal= "10", "" # número a convertir a hexadecimal y variable para almacenar el resultado
+if numero == "0":  print("0") # si el número es 0, imprimir 0
+digitos = "0123456789ABCDEF" # lista de dígitos hexadecimales
+while numero > "0": hexadecimal, numero = digitos[int(numero) % 16] + hexadecimal, str(int(numero) // 16) # mientras el número sea mayor que 0, calcular el residuo de la división entre 16 y agregarlo al inicio de la cadena hexadecimal, luego dividir el número entre 16
+print(hexadecimal) # imprimir el resultado final
